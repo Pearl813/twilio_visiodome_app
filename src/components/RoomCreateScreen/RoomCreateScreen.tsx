ff9810af9e781daff3da861ecd85cbfd68a95fdd
AlbertG0813 <kinddev813@gmail.com>
2023-07-03 00:12:06 -0400

    sd

--- src/components/RoomCreateScreen/RoomCreateScreen.tsx
+++ src/components/RoomCreateScreen/RoomCreateScreen.tsx
@@ -101,7 +101,7 @@ export default function RoomCreateScreen() {
       axios
         .post(`${process.env.REACT_APP_TOKEN_SERVER_URL}/room/end`, {}, { headers })
         .then(response => {
-          if (response.data.code === RESULT_CODE_SUCCESS) {
+          if (response.data.code === RESULT_CODE_SUCCESS || response.data.code === -1) {
             setIsOpen(true);
             setMessageHeader('Success!');
             setMessageContent('Room Closed Successfully.');
