d80be241134af7e7accdff6f42ee26b3a352f6fe
AlbertG0813 <kinddev813@gmail.com>
2023-07-03 20:34:32 -0400

    asd

--- src/components/Room/Room.tsx
+++ src/components/Room/Room.tsx
@@ -105,7 +105,7 @@ export default function Room() {
       ) : (
         <>
           <MainParticipant />
-          {/* <ParticipantList /> */}
+          <ParticipantList />
         </>
       )}
 
