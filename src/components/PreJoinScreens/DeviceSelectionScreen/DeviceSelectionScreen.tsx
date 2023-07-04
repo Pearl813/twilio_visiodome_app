80d4d49094773205d435cee2f904be5632c4c261
AlbertG0813 <kinddev813@gmail.com>
2023-07-04 11:05:50 -0400

    c

--- src/components/PreJoinScreens/DeviceSelectionScreen/DeviceSelectionScreen.tsx
+++ src/components/PreJoinScreens/DeviceSelectionScreen/DeviceSelectionScreen.tsx
@@ -95,7 +95,7 @@ export default function DeviceSelectionScreen({ name, roomName, isPresenter, set
   const disableButtons = isFetching || isAcquiringLocalTracks || isConnecting;
   const [isLoading, setIsLoading] = useState(false);
   const [isInvalidRoom, setIsInvalidRoom] = useState(false);
-  // const [hasJoined, setHasJoined] = useState(false);
+  const [hasJoined, setHasJoined] = useState(false);
 
   const localVideoTrack = localTracks.find(track => track.kind === 'video') as LocalVideoTrack | undefined;
   const localAudioTrack = localTracks.find(track => track.kind === 'audio') as LocalAudioTrack;
@@ -129,11 +129,11 @@ export default function DeviceSelectionScreen({ name, roomName, isPresenter, set
             const audioDevice = audioInputDevices.find(device => device.label === DEFAULT_AUDIO_DEVICE_LABEL);
             if (audioDevice?.deviceId) {
               console.log(videoDevice.deviceId, ':::', audioDevice.deviceId);
-              // if (hasJoined === false) {
-              replaceTrack(videoDevice.deviceId, audioDevice.deviceId);
-              handleJoin();
-              //   setHasJoined(true);
-              // }
+              if (hasJoined === false) {
+                replaceTrack(videoDevice.deviceId, audioDevice.deviceId);
+                handleJoin();
+                setHasJoined(true);
+              }
             } else {
               console.log('audio device not found');
               setIsLoading(false);
