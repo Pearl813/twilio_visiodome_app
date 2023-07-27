16f82327cd368db50cb2ecab924553d19c240a6b
AlbertG0813 <kinddev813@gmail.com>
2023-07-27 05:55:58 -0400

    sc

--- src/components/AuthRoute/index.tsx
+++ src/components/AuthRoute/index.tsx
@@ -7,7 +7,10 @@ export const AuthRoute = ({ children, ...rest }: RouteProps) => {
 
   const isAuthReady = validUser || isValidating;
 
-  console.log('---------', authUser, validUser, isValidating, isAuthReady);
+  if (!authUser && !isAuthReady) {
+    return null;
+  }
+
   return (
     <Route
       {...rest}
