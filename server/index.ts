092b03030cfd4247b45840cace2e59300a93194f
AlbertG0813 <kinddev813@gmail.com>
2023-08-01 03:21:34 -0400

    d

--- server/index.ts
+++ server/index.ts
@@ -1,7 +1,7 @@
 import './bootstrap-globals';
 import { createExpressHandler } from './createExpressHandler';
 import { startRoom, endRoom, validateRoom, getRoomLinks } from './handlers/room';
-import { validatePresenter, validateToken, login } from './handlers/user';
+import { validatePresenter, validateToken, login, generateRoomName } from './handlers/user';
 import express, { RequestHandler } from 'express';
 import path from 'path';
 import { ServerlessFunction } from './types';
