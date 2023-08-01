1760661bafd6a8ed56d7b2a5991dc313ed9adf01
AlbertG0813 <kinddev813@gmail.com>
2023-08-01 03:45:12 -0400

    s

--- src/components/RoomCreateScreen/RoomCreateScreen.tsx
+++ src/components/RoomCreateScreen/RoomCreateScreen.tsx
@@ -58,7 +58,7 @@ export default function RoomCreateScreen() {
       const headers = {
         Authorization: `Bearer ${authUser.token}`,
       };
-      axios.get(`${process.env.REACT_APP_STRAPI_URL}/user/generateRoomName`, { headers }).then(response => {
+      axios.get(`${process.env.REACT_APP_TOKEN_SERVER_URL}/user/generateRoomName`, { headers }).then(response => {
         if (response.status === 200) {
           if (response.data.code === RESULT_CODE_SUCCESS) {
             axios
